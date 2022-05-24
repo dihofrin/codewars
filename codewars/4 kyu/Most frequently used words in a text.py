185f443721d38e9ecd3a5942129472aa0ed1a9a5
Robert Ford <serhiopich@gmail.com>
2022-05-24 15:17:51 +0300

    24.05.2022

--- codewars/4 kyu/Most frequently used words in a text.py
+++ codewars/4 kyu/Most frequently used words in a text.py
@@ -1,11 +1,39 @@
+"""Write a function that, given a string of text (possibly with punctuation and line-breaks), returns an array of the top-3 most occurring words, in descending order of the number of occurrences.
+
+Assumptions:
+A word is a string of letters (A to Z) optionally containing one or more apostrophes (') in ASCII.
+Apostrophes can appear at the start, middle or end of a word ('abc, abc', 'abc', ab'c are all valid)
+Any other characters (e.g. #, \, / , . ...) are not part of a word and should be treated as whitespace.
+Matches should be case-insensitive, and the words in the result should be lowercased.
+Ties may be broken arbitrarily.
+If a text contains fewer than three unique words, then either the top-2 or top-1 words should be returned, or an empty array if a text contains no words.
+Examples:
+top_3_words("In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.")
+# => ["a", "of", "on"]
+
+top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")
+# => ["e", "ddd", "aa"]
+
+top_3_words("  //wont won't won't")
+# => ["won't", "wont"]
+Bonus points (not really, but just for fun):
+Avoid creating an array whose memory footprint is roughly as big as the input text.
+Avoid sorting the entire array of unique words."""
+
+
 def top_3_words(text):
-    text = text.strip(' ,#/\, ').split()
+    text = ''.join(i for i in text if i.isalpha() or i in '\' ').split()
     result = {}
     for i in text:
         result[i] = result.get(i, 0) + 1
     sorted_keys = sorted(result.items(), key=lambda x: (-x[1], x[0]))
     sorted_result = dict(sorted_keys)
-    final = [i.lower() for i in list(sorted_result.keys())]
+    final = [i.lower() for i in list(sorted_result.keys()) if i not in "'''"]
     return final[:3]
 
-print(top_3_words('e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e'))
+print(top_3_words(""))
