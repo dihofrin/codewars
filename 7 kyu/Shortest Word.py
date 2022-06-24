0c3ba3a03e7fa0f7c0f0690f00808493e1a4349c
dihofrin <serhiopich@gmail.com>
2022-06-24 23:40:09 +0300

    24062022

--- 7 kyu/Shortest Word.py
+++ 7 kyu/Shortest Word.py
@@ -7,5 +7,3 @@ String will never be empty and you do not need to account for different data typ
 def find_short(s):
     # your code here
     return len(sorted(s.split(), key=len)[0])
-
-print(find_short('pizda dhigurda huy'))
