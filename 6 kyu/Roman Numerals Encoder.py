bf66d86c3244eed8ea4dab94a20927fd270968c4
dihofrin <serhiopich@gmail.com>
2022-06-28 23:55:16 +0300

    28062022

---
+++ 6 kyu/Roman Numerals Encoder.py
@@ -0,0 +1,42 @@
+"""Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+
+Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+Example:
+
+solution(1000) # should return 'M'
+Help:
+
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000
+Remember that there can't be more than 3 identical symbols in a row.
+
+More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals"""
+
+def solution(n):
+    symbols = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M', }
+    if n in symbols:
+        return symbols[n]
+
+    numbers = sorted(symbols)
+    s = []
+    z = -1
+    result = ''
+    while n != 0:
+        tmp = numbers[z]
+        if n >= tmp:
+            n -= tmp
+            s.append(tmp)
+        else:
+            z -= 1
+    for i in s:
+        result += symbols[i]
+    return result
+
+print(solution(1994))
